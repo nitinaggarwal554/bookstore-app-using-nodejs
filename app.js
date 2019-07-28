d695f6280945ee46dfd3f1e2710598ed038a92dd
nitinaggarwal554 <nitinaggarwal554@gmail.com>
2019-07-29 02:34:30 +0530

    bookstore using node.js.

---
+++ app.js
@@ -0,0 +1,43 @@
+var express=require('express');
+var favicon=require('serve-favicon');
+var bodyParser=require('body-parser');
+var morgan=require('morgan');
+var path=require('path');
+var book=require('./routes/book');
+var auth=require('./routes/auth');
+var mongoose=require('mongoose');
+mongoose.Promise=require('bluebird');
+mongoose.connect('mongodb://localhost:27017/adminportaldb',{
+    useNewUrlParser:true,
+    promiseLibrary:require('bluebird')})
+    .then(()=> console.log('connection is successful'))
+    .catch((err) => console.error(err));
+
+
+var app= express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use('/api/book', book);
+app.use('/api/auth',auth);
+
+app.use(function(req,res,next){
+    var err=new Error('Not Found');
+    err.status=404;
+    next(err); 
+})
+
+app.use(function(err,req,res,next){
+    //set locals to providing errors in development
+    res.locals.message=err.message;
+    res.locals.error=req.app.get('env') === 'development' ? err : {};
+
+    //render the error page
+    res.status(err.status || 500);
+    res.send('error');
+
+});
+module.exports=app;
---
+++ build/precache-manifest.8f3606391166a4e839856ddc8c3e8783.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51adb4090c70eb2ee6645d5999fc80a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f44140faaa5d26a04a9d",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "1ad024696f866a757543",
+    "url": "/static/css/main.f9d3dd47.chunk.css"
+  },
+  {
+    "revision": "f44140faaa5d26a04a9d",
+    "url": "/static/js/2.1597286c.chunk.js"
+  },
+  {
+    "revision": "1ad024696f866a757543",
+    "url": "/static/js/main.13c7eb57.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
